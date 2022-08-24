941e67dea47237d54fe87be41eedd616d0ba4615
SerhiiDubovets <Sergio.d@bigmir.net>
2022-08-24 21:35:53 +0300

    1

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
@@ -17,7 +18,14 @@ const register = createAsyncThunk('/auth/register', async credentials => {
     token.set(data.token);
     return data;
   } catch (error) {
-    console.log(error);
+    toast(`Регистрация не удалась. Попробуйте ещё.`, {
+      icon: '😅',
+      duration: 6000,
+      style: {
+        border: '3px solid orange',
+      },
+    });
+    // console.log(error);
   }
 });
 const logIn = createAsyncThunk('/auth/login', async credentials => {
@@ -26,7 +34,13 @@ const logIn = createAsyncThunk('/auth/login', async credentials => {
     token.set(data.token);
     return data;
   } catch (error) {
-    console.log(error);
+    toast(`Неверная почта или пароль.`, {
+      icon: '😅',
+      duration: 6000,
+      style: {
+        border: '3px solid orange',
+      },
+    });
   }
 });
 const logOut = createAsyncThunk('/auth/logout', async () => {
